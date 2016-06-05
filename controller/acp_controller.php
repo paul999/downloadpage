b0f11b64da1d77c43a2832c4836ad8ec5c061fe5
Paul Sohier <paul999@phpbb.com>
2016-06-05 14:06:24 +0200

    Some more setup for acp controller

--- controller/acp_controller.php
+++ controller/acp_controller.php
@@ -10,9 +10,32 @@
 
 namespace paul999\downloadpage\controller;
 
+use phpbb\db\driver\driver_interface;
+use phpbb\files\upload;
+use phpbb\request\request_interface;
+use phpbb\template\template;
 
 class acp_controller
 {
+    /**
+     * @var request_interface
+     */
+    private $request;
+
+    /**
+     * @var driver_interface
+     */
+    private $db;
+
+    /**
+     * @var template
+     */
+    private $template;
+
+    /**
+     * @var upload
+     */
+    private $upload;
 
     /**
      * @var string
@@ -20,17 +43,87 @@ class acp_controller
     private $u_action;
 
     /**
-     * acp_controller constructor.
-     *
+     * @var string
      */
-    public function __construct()
+    private $downloads_table;
+
+    /**
+     * @var string
+     */
+    private $versions_table;
+
+    /**
+     * @var string
+     */
+    private $releases_table;
+
+    /**
+     * acp_controller constructor.
+     * @param request_interface $request
+     * @param driver_interface $db
+     * @param template $template
+     * @param upload $upload
+     * @param string $downloads_table
+     * @param string $versions_table
+     * @param string $releases_table
+     */
+    public function __construct(request_interface $request, driver_interface $db, template $template, upload $upload, $downloads_table, $versions_table, $releases_table)
     {
+        $this->request          = $request;
+        $this->downloads_table  = $downloads_table;
+        $this->versions_table   = $versions_table;
+        $this->releases_table   = $releases_table;
+        $this->db               = $db;
+        $this->upload           = $upload;
+        $this->template         = $template;
+    }
+
+    /**
+     * @return void
+     */
+    public function versions() {
+        $action = $this->request->variable('action', '');
+
+        switch ($action) {
+            case 'order':
+                $this->orderVersions();
+                break;
+            case 'add':
+                $this->addVersion();
+                break;
+            case 'addNew':
+                $this->addNewVersion();
+                break;
+            default:
+                $this->versionsIndex();
+        }
     }
 
     /**
      *
      */
-    public function versions() {
+    private function versionsIndex() {
+
+    }
+
+    /**
+     *
+     */
+    private function orderVersions() {
+
+    }
+
+    /**
+     *
+     */
+    private function addVersion() {
+
+    }
+
+    /**
+     *
+     */
+    private function addNewVersion() {
 
     }
 
