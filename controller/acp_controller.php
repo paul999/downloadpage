3104d4856751981cd8f47152b8b8cece7eefa70a
Paul Sohier <paul999@phpbb.com>
2016-12-27 13:14:08 +0100

    Add new version

--- controller/acp_controller.php
+++ controller/acp_controller.php
@@ -10,9 +10,13 @@
 
 namespace paul999\downloadpage\controller;
 
+use phpbb\console\command\user\add;
 use phpbb\db\driver\driver_interface;
+use phpbb\exception\http_exception;
 use phpbb\files\upload;
 use phpbb\json_response;
+use phpbb\language\language;
+use phpbb\log\log_interface;
 use phpbb\request\request_interface;
 use phpbb\template\template;
 
@@ -57,6 +61,14 @@ class acp_controller
      * @var string
      */
     private $releases_table;
+    /**
+     * @var language
+     */
+    private $lang;
+    /**
+     * @var log_interface
+     */
+    private $log;
 
     /**
      * acp_controller constructor.
@@ -64,11 +76,13 @@ class acp_controller
      * @param driver_interface $db
      * @param template $template
      * @param upload $upload
+     * @param language $lang
+     * @param log_interface $log
      * @param string $downloads_table
      * @param string $versions_table
      * @param string $releases_table
      */
-    public function __construct(request_interface $request, driver_interface $db, template $template, upload $upload, $downloads_table, $versions_table, $releases_table)
+    public function __construct(request_interface $request, driver_interface $db, template $template, upload $upload, language $lang, log_interface $log, $downloads_table, $versions_table, $releases_table)
     {
         $this->request          = $request;
         $this->downloads_table  = $downloads_table;
@@ -77,6 +91,8 @@ class acp_controller
         $this->db               = $db;
         $this->upload           = $upload;
         $this->template         = $template;
+        $this->lang = $lang;
+        $this->log = $log;
     }
 
     /**
@@ -89,16 +105,12 @@ class acp_controller
         switch ($action) {
             case 'order':
                 $this->orderVersions($id, $this->request->variable('up', false));
-                break;
             case 'add':
                 $this->addVersion();
-                break;
             case 'active':
                 $this->activate($id, $this->request->is_set('active'));
-                break;
             case 'addRelease':
                 $this->createNewRelease();
-                break;
             default:
                 $this->versionsIndex();
         }
@@ -128,7 +140,10 @@ class acp_controller
 
         $this->template->assign_vars(array(
             'VERSIONS_MAIN' => true,
+            'U_ACTION'      => $this->u_action,
         ));
+
+        add_form_key('download_page_version');
     }
 
     /**
@@ -155,7 +170,29 @@ class acp_controller
      *
      */
     private function addVersion() {
+        if (!check_form_key('download_page_version')) {
+            $this->template->assign_var('ERROR_MSG', $this->lang->lang('FORM_INVALID'));
+            return;
+        }
+        $name = $this->request->variable('name', '');
 
+        if (empty($name)) {
+            $this->template->assign_var('ERROR_MSG', $this->lang->lang('MISSING_VERSION_NAME'));
+            return;
+        }
+        $sql = 'SELECT MAX(sort) as mx FROM ' . $this->versions_table;
+        $result = $this->db->sql_query($sql);
+        $max = (int) $this->db->sql_fetchfield('mx', $result);
+        $this->db->sql_freeresult($result);
+
+        $sql_ary = [
+            'active'        => false,
+            'name'          => $name,
+            'sort'          => $max + 1,
+            'version_time'  => time(),
+        ];
+        $sql = 'INSERT INTO ' . $this->versions_table . ' ' . $this->db->sql_build_array('INSERT', $sql_ary);
+        $this->db->sql_query($sql);
     }
 
     /**
